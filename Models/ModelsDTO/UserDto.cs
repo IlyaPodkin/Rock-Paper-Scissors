a996f492b99022949b3b6460ba329c0e08bb56bd
Илья Подкин <144801578+IlyaPodkin@users.noreply.github.com>
2025-02-05 23:10:37 +0400

    Delete Models directory

--- Models/ModelsDTO/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GrpcService.Models.ModelsDTO
-{
-    public class UserDto
-    {
-        public string Name { get; set; }
-        public decimal Balance { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
-}
